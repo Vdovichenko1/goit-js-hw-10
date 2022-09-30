e58782b826cf89ad0c9996b169b993129d9009b4
Anton Vdovichenko <106983322+Vdovichenko1@users.noreply.github.com>
2022-09-30 10:31:38 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener('input', fetchCountries);
+
+// function inputCountries() {}
+
+function fetchCountries(name) {
+  fetch('https://restcountries.com/v2/all?fields=uk')
+    .then(response => {
+      return response.json();
+    })
+    .then(country => {
+      console.log(country);
+      const markup = `<h2>${name.official}</h2>
+<p>Capital: ${capital}</p>
+<p>Population: ${population}</p>
+<p>Languages: ${languages}</p>`;
+      refs.list.innerHTML = markup;
+      console.log(markup);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+// function countryCard(country) {
+//   return `<h2>${flags.svg} ${name.official}</h2>
+// <p>Capital: ${capital}</p>
+// <p>Population: ${population}</p>
+// <p>Languages: ${languages}</p>`;
+// }
